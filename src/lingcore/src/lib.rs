ee2ff4c763a4f9e8ef48168f4e580b632ff6068f
Ruoqing He <ruoqing.he@lingcage.com>
2026-03-02 14:31:56 +0000

    lingcage: Convert to a workspace
    Convert the package specification to workspace specification, since we
    will be using lingcage as our monorepo to contain all lingcage
    components.
    
    Signed-off-by: Ruoqing He <ruoqing.he@lingcage.com>

---
+++ src/lingcore/src/lib.rs
@@ -0,0 +1,18 @@
+// SPDX-FileCopyrightText: 2026 LingCage <opensource@lingcage.com>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
--- src/main.rs
+++
@@ -1,7 +0,0 @@
-// SPDX-FileCopyrightText: 2026 LingCage <opensource@lingcage.com>
-//
-// SPDX-License-Identifier: Apache-2.0
-
-fn main() {
-    println!("Hello, world!");
-}
